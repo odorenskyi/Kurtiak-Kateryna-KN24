3c76e7ee5173858d8cc795db3bfbb7dcc5c3658c
Kurtiak Katerina <katerinak2503@gmail.com>
2025-03-28 12:26:34 +0300

    Delete lab08/Modules_Kurtiak directory

--- lab08/Modules_Kurtiak/ModulesK.h
+++
@@ -1,6 +0,0 @@
-#ifndef MODULESK_H_INCLUDED
-#define MODULESK_H_INCLUDED
-
-double s_calculation(double x, double y, double z);
-
-#endif // MODULESK_H_INCLUDED
--- lab08/Modules_Kurtiak/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-#include <stdexcept>
-
-// Функція для обчислення S відповідно до формули варіанту 25
-double s_calculation(double x, double y, double z) {
-    return fabs(sqrt(fabs(z - 2 * x)) - x * sin(pow(x, 2 * z) * y));
-}
