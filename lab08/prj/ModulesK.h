b582436e0aba1ea80d564411204dc0d4ceec4112
fjgevc <katerinak2503@gmail.com>
2025-04-04 17:44:23 +0300

    added functions to the implementation and recorded them in a .h file

--- lab08/prj/ModulesK.h
+++ lab08/prj/ModulesK.h
@@ -1,6 +1,12 @@
-#ifndef MODULESK_H_INCLUDED
-#define MODULESK_H_INCLUDED
+#ifndef MODULESKURTIAK_H_INCLUDED
+#define MODULESKURTIAK_H_INCLUDED
+
+#include <string>
 
 double s_calculation(double x, double y, double z);
+int countBits(int N);
+std::string task_9_1(double s_v);
 
-#endif // MODULESK_H_INCLUDED
+double celsiusToFahrenheit(double celsius);
+
+#endif // MODULESKURTIAK_H_INCLUDED
