06121f766075b21470706b7d1065a5a37b37c008
fjgevc <katerinak2503@gmail.com>
2025-04-04 17:22:41 +0300

    Виправила помилку в сатистичній бібліотеці

--- lab08/prj/ModulesKurtiak/main.cpp
+++ lab08/prj/ModulesKurtiak/main.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <stdexcept>
 
-// Функція для обчислення S відповідно до формули варіанту 25
 double s_calculation(double x, double y, double z) {
     return fabs(sqrt(fabs(z - 2 * x)) - x * sin(pow(x, 2 * z) * y));
 }
